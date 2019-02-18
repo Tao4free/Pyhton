266ae251f637a3709b3a85439e3ba1707fc4606d
tao4free <takizawa_tao@live.com>
2019-02-18 17:54:12 +0900

    test

--- work_with_excel/read_copy_export/read_copy_export.py
+++ work_with_excel/read_copy_export/read_copy_export.py
@@ -1,43 +1,53 @@
 #!/usr/local/bin/python3
+
 import os
 from openpyxl import load_workbook
 
-wb = load_workbook(filename='sample.xlsx', read_only=True)
+outdir = "output"
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
 
-for sheet in wb.worksheets:
-    title = sheet.title
-    mxrow = sheet.max_row
-    print(title,mxrow)
+indir = "input"
+infiles = os.listdir(indir)
 
-    outdir = "output"
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+for inf in infiles:
+    #print(inf)
+    inf_path = indir + "/" + inf
+
+    wb = load_workbook(filename=inf_path, read_only=True)
     
-    of = outdir + '/' + title + '.txt'
-    f = open(of, 'w')
+    for sheet in wb.worksheets:
+        title = sheet.title
+        mxrow = sheet.max_row
+        print(inf, " | ", title)#,mxrow)
+        
+        of = outdir + '/' + title + '.txt'
+        f = open(of, 'w')
+    
+        for nrow in range(4,mxrow):
+            #print(irow)
+            for ncol in range(3,6):
+                cellValue = sheet.cell(row=nrow, column=ncol).value
+                if cellValue == None:
+                    break
+                else:
+                    cellValue = round(cellValue, 4)
+                if ncol == 3:
+                    #print("{0:10.2f}".format(cellValue), end='')
+                    cellValue = '%.2f' % cellValue
+                    str1 = str(cellValue)
+                if ncol == 4:
+                    #print("{0:10.3f}".format(cellValue), end='') 
+                    cellValue = '%.3f' % cellValue
+                    str2 = str(cellValue)
+                if ncol == 5:
+                    #print("{0:5d}".format(cellValue)) 
+                    cellValue = '%d' % cellValue
+                    str3 = str(cellValue)
+            line     = '{:>10s} {:>10s} {:>5s}\n'.format(str1, str2, str3)
+            f.write(line)
+    
+        f.close()
 
-    for nrow in range(4,mxrow):
-        #print(irow)
-        for ncol in range(3,6):
-            cellValue = sheet.cell(row=nrow, column=ncol).value
-            if cellValue == None:
-                break
-            else:
-                cellValue = round(cellValue, 4)
-            if ncol == 3:
-                #print("{0:10.2f}".format(cellValue), end='')
-                cellValue = '%.2f' % cellValue
-                str1 = str(cellValue)
-            if ncol == 4:
-                #print("{0:10.3f}".format(cellValue), end='') 
-                cellValue = '%.3f' % cellValue
-                str2 = str(cellValue)
-            if ncol == 5:
-                #print("{0:5d}".format(cellValue)) 
-                cellValue = '%d' % cellValue
-                str3 = str(cellValue)
-        line     = " %s   %s   %s\n" % (str1, str2, str3)
-        f.write(line)
-
-    f.close()
+    wb._archive.close()
 
