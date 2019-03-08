b77dbc150182e794255d2a98db34bbd6fc1563c9
tao4free <takizawa_tao@live.com>
2019-03-08 20:56:52 +0900

    test

--- work_with_excel/read_copy_export/read_copy_export.py
+++ work_with_excel/read_copy_export/read_copy_export.py
@@ -3,6 +3,32 @@
 import os
 from openpyxl import load_workbook
 
+
+def myfunc:
+    for nrow in range(4,mxrow):
+        #print(irow)
+        for ncol in range(3,6):
+            cellValue = sheet.cell(row=nrow, column=ncol).value
+            if cellValue == None:
+                return
+            else:
+                cellValue = round(cellValue, 4)
+            if ncol == 3:
+                #print("{0:10.2f}".format(cellValue), end='')
+                cellValue = '%.2f' % cellValue
+                str1 = str(cellValue)
+            if ncol == 4:
+                #print("{0:10.3f}".format(cellValue), end='') 
+                cellValue = '%.3f' % cellValue
+                str2 = str(cellValue)
+            if ncol == 5:
+                #print("{0:5d}".format(cellValue)) 
+                cellValue = '%d' % cellValue
+                str3 = str(cellValue)
+        line     = '{:>10s} {:>10s} {:>5s}\n'.format(str1, str2, str3)
+        f.write(line)
+
+
 outdir = "output"
 if not os.path.exists(outdir):
     os.makedirs(outdir)
@@ -24,28 +50,7 @@ for inf in infiles:
         of = outdir + '/' + title + '.txt'
         f = open(of, 'w')
     
-        for nrow in range(4,mxrow):
-            #print(irow)
-            for ncol in range(3,6):
-                cellValue = sheet.cell(row=nrow, column=ncol).value
-                if cellValue == None:
-                    break
-                else:
-                    cellValue = round(cellValue, 4)
-                if ncol == 3:
-                    #print("{0:10.2f}".format(cellValue), end='')
-                    cellValue = '%.2f' % cellValue
-                    str1 = str(cellValue)
-                if ncol == 4:
-                    #print("{0:10.3f}".format(cellValue), end='') 
-                    cellValue = '%.3f' % cellValue
-                    str2 = str(cellValue)
-                if ncol == 5:
-                    #print("{0:5d}".format(cellValue)) 
-                    cellValue = '%d' % cellValue
-                    str3 = str(cellValue)
-            line     = '{:>10s} {:>10s} {:>5s}\n'.format(str1, str2, str3)
-            f.write(line)
+        myfunc()
     
         f.close()
 
