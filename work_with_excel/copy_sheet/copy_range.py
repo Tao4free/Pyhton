c82bde34a50c8b9766e487954f0221771ec41529
tao4free <takizawa_tao@live.com>
2019-03-11 15:35:58 +0900

    test

--- work_with_excel/copy_sheet/copy_range.py
+++ work_with_excel/copy_sheet/copy_range.py
@@ -13,10 +13,10 @@ ws_ref = wb_ref["refer"]
 
 wb = load_workbook(filename=inf)
 
-start_no = 286
-start_dis = 57.2
+start_no = 291
+start_dis = 58.2
 
-for row in range(1,12):#ws_ref.max_row):
+for row in range(1,ws_ref.max_row):
     nowrow = ws_ref.cell(row,1).value
     nextrow = ws_ref.cell(row+1,1).value
     if (nowrow is None and nextrow is None):
@@ -24,21 +24,24 @@ for row in range(1,12):#ws_ref.max_row):
     if ('k' in str(nowrow)):
         nowrow = str(nowrow).split('k')
         nowrow = nowrow[0]
-        nowrow = float(nowrow)
+        nowrow = round(float(nowrow),1)
 
-        no = int( (nowrow - start_dis) / 0.2 + 286 )
+        no = int( round((nowrow - start_dis),1) / 0.2 + start_no )
         noname = "No." + str(no)
+        if (no < 291):
+            continue
+        if (no > 354):
+            continue
+        if (no%5 == 0):
+            continue
 
         nownum = ws_ref.cell(row,2).value
         nextnum = ws_ref.cell(row+1,2).value
 
         #print(nowrow, ":  ", noname, ":  ", nownum, ":  ", nextnum)
+        print(nowrow,no,noname)
 
         ws = wb[noname]
-        #v1 = ws.cell(9,3).value
-        #v2 = ws.cell(9,4).value
-        #v3 = ws.cell(9,2).value
-        #print("%.2f %.3f %s" % (v1, v2, v3))
 
         for n in range(3, 3+nownum):
             dis = float(ws_ref.cell(row,n).value)
@@ -66,45 +69,4 @@ for row in range(1,12):#ws_ref.max_row):
 wb.save(inf)
 
 print("Finish!", end='')
-
-#for inf in infiles:
-#    #print(inf)
-#    inf_path = indir + "/" + inf
-#
-#    wb = load_workbook(filename=inf_path, read_only=True, data_only=True)
-#    
-#    for sheet in wb.worksheets:
-#        title = sheet.title
-#        mxrow = sheet.max_row
-#        print(inf, " | ", title)#,mxrow)
-#        
-#        of = outdir + '/' + title + '.txt'
-#        f = open(of, 'w')
-#    
-#        for nrow in range(4,mxrow):
-#            #print(irow)
-#            for ncol in range(3,6):
-#                cellValue = sheet.cell(row=nrow, column=ncol).value
-#                if cellValue == None:
-#                    break
-#                else:
-#                    cellValue = round(cellValue, 4)
-#                if ncol == 3:
-#                    #print("{0:10.2f}".format(cellValue), end='')
-#                    cellValue = '%.2f' % cellValue
-#                    str1 = str(cellValue)
-#                if ncol == 4:
-#                    #print("{0:10.3f}".format(cellValue), end='') 
-#                    cellValue = '%.3f' % cellValue
-#                    str2 = str(cellValue)
-#                if ncol == 5:
-#                    #print("{0:5d}".format(cellValue)) 
-#                    cellValue = '%d' % cellValue
-#                    str3 = str(cellValue)
-#            line     = '{:>10s} {:>10s} {:>5s}\n'.format(str1, str2, str3)
-#            f.write(line)
-#    
-#        f.close()
-#
-#    wb._archive.close()
 
