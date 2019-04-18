989e8145f723c82f9d0ef3b8dbc1d54d65fc724f
tao4free <takizawa_tao@live.com>
2019-04-18 18:01:39 +0900

    test

--- work_with_gis/SuperLabeling/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
- This script initializes the plugin, making it known to QGIS.
-"""
-from __future__ import absolute_import
-
-def classFactory(iface):
-    from .SuperLabeling import SuperLabeling
-    return SuperLabeling(iface)
