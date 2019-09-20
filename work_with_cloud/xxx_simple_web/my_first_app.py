b3891ea68129304f53ab0a89b2b13b787f71e276
盧　涛 <lu.tao@CA-PC-0059.local>
2019-09-20 12:25:18 +0900

    test

---
+++ work_with_cloud/xxx_simple_web/my_first_app.py
@@ -0,0 +1,8 @@
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.header['Content-type'] = 'text/plain'
+        self.response.write('Hello, World! From the folks at Loonycorn!')
+
+app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
