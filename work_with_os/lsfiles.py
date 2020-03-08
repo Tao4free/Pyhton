4025568d3c9abfbd78c60c551841311f66a13a64
tao4free <tao4free@gmail.com>
2020-03-08 22:09:33 +0900

    add

--- work_with_os/lsfiles.py
+++ work_with_os/lsfiles.py
@@ -1,18 +1,60 @@
 import os
 import time
 from pprint import pprint
+import shutil
 
-path = '/Users/lu.tao/Documents/python2_virtualenv'
+path = "/c/Users/takiz/Pictures/QinDi/VUE"
+path = 'TaoLU'
+path = "c:\\Users\\takiz\\Pictures\\QinDi\\VUE"
+path = "c:\\Users\\takiz\\Pictures\\rmQinDi"
 
 filedict = {}
-for idx, (dirpath, dirnames, filenames) in enumerate(os.walk(path), 1):
+rmlist = []
+for (dirpath, dirnames, filenames) in os.walk(path):
     if filenames:
+        # print("\n".join(filenames))
         for fn in filenames:
             filepath = os.path.join(dirpath, fn)
-            if os.path.isfile(filepath) and  not os.access(filepath, os.X_OK):
-                ctime = time.strftime('%Y/%m/%d_%H:%M:%S', time.localtime(os.stat(filepath).st_ctime))
+            s = os.stat(filepath)
+            ctime = s.st_size
+            ctime = time.strftime('%Y/%m/%d_%H:%M:%S', time.localtime(s.st_ctime))
+            # print(fn, s.st_size, ctime)
+            if ctime in filedict:
+                filepath_pre = filedict[ctime]
+                # print("yes", fn)
+                # print(len(filepath_pre), len(filepath))
+                # print(filepath_pre, "\n", filepath)
+                if len(filepath_pre) < len(filepath):
+                    # print("len pre < len")
+                    # print("filepath_pre: ", filepath_pre)
+                    # print("filepath: ", filepath)
+                    rmlist.append(filepath)
+                elif len(filepath_pre) > len(filepath):
+                    rmlist.append(filepath_pre)
+                elif len(filepath_pre) == len(filepath):
+                    if filepath_pre < filepath:
+                        # print("pre < ")
+                        # print("filepath_pre: ", filepath_pre)
+                        # print("filepath: ", filepath)
+                        rmlist.append(filepath)
+            else:
                 filedict.update({ctime: filepath})
-                # print("creation time of {}:\n    {}".format(filepath, ctime))
+            # print(fn, os.path.isfile(filepath), os.access(filepath, os.X_OK))
+            # if  os.path.isfile(filepath) and  not os.access(filepath, os.X_OK):
+            # ctime = time.strftime('%Y/%m/%d_%H:%M:%S', time.localtime(os.stat(filepath).st_ctime))
+            # ctime = time.strftime('%Y/%m/%d_%H:%M:%S', time.localtime(os.path.getctime(filepath)))
+            # print(ctime)
 
-pprint(filedict)
-# print(filedict)
+# pprint(filedict)
+# pprint(rmlist)
+rmlist = list(dict.fromkeys(rmlist))
+for rml in rmlist:
+    spt01 = rml.split("rmQinDi")
+    spt02 = rml.split("\\")
+    target = spt01[0] + "\\rmQinDi2\\" + spt02[-1]
+    # # print(target)
+    # # try:
+    shutil.move(rml, target)
+    # # except:
+        # # pass
+# # print(len(rmlist))
